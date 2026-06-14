578e0f05a3856beb7fecf539c0c0f110c8fc4aa8
Shobha Venkatesh <132358930+shobhavk@users.noreply.github.com>
2026-06-14 23:38:39 +0530

    Enable sharing option in UI launch

--- app.py
+++ app.py
@@ -266,6 +266,6 @@ if __name__ == "__main__":
     ui.launch(
         server_name="0.0.0.0",
         server_port=int(os.getenv("PORT", 7860)),
-        share=False,
+        share=True,
         show_error=True,
     )
